7eb20578e093e646a70a2bf031cc029d60dc201a
Michael Roussel <pokemon_pidgeot@msn.com>
2019-05-23 16:50:58 -0400

    Dynamic port assignment

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.get(`/`, (req, res) => {
 app.get(`/about`, (req, res) => {
     res.send(`I like long walks on the beach`);
 })
-
-app.listen(4000, () => console.log(`Listening on 4000`));
+const port = (process.emv.PORT || 4000);
+app.listen((process.emv.PORT || 4000), () => console.log(`Listening on ${port}`));
