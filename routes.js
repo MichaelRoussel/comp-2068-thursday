7b9636d67c4db498d33147d0a5246b17b947d296
Michael Roussel <pokemon_pidgeot@msn.com>
2019-06-20 08:02:59 -0400

    past 3 weeks lmao

--- routes.js
+++ routes.js
@@ -3,8 +3,14 @@ const app = express();
 
 // import our page routes
 const pageRoutes = require('./routes/pages');
+const blogsRoutes = require('./routes/blogs');
+const authorsRoutes = require('./routes/authors');
+const sessionsRoutes = require('./routes/sessions');
 
 app.use('/', pageRoutes);
+app.use('/blogs', blogsRoutes);
+app.use('/authors', authorsRoutes);
+app.use('/', sessionsRoutes);
 
 //export the changes
 module.exports = app;
